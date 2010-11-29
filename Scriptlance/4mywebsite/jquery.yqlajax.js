c189f471191ff3d43aa528bbb5628ea7e5d39b4a
immabear <crackopenabear@hotmail.com>
2010-11-28 19:51:39 -0800

    x

--- Scriptlance/4mywebsite/jquery.yqlajax.js
+++ Scriptlance/4mywebsite/jquery.yqlajax.js
@@ -38,10 +38,7 @@ jQuery.ajax = (function(_ajax){
                     if (_success) {
                         // Fake XHR callback.
                         _success.call(this, {
-                            responseText: data.results.join(" ")
-                                // YQL screws with <script>s
-                                // Get rid of them
-                                .replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi, '')
+                            responseText: data.results.Result.join(" ")
                         }, 'success');
                     }
                     
