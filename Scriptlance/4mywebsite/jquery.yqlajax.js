afbaa3bd93725e081adfba8ffcaa7ceb94c0574c
immabear <crackopenabear@hotmail.com>
2010-11-28 19:35:51 -0800

    x

---
+++ Scriptlance/4mywebsite/jquery.yqlajax.js
@@ -0,0 +1,58 @@
+jQuery.ajax = (function(_ajax){
+
+    var protocol = location.protocol,
+        hostname = location.hostname,
+        exRegex = RegExp(protocol + '//' + hostname),
+        YQL = 'http' + (/^https/.test(protocol)?'s':'') + '://query.yahooapis.com/v1/public/yql?format=json&callback=?',
+        query = "select {SELECT} from {SEARCHTYPE} where query='{QUERY}'";
+		
+    function isExternal(url) {
+        return !exRegex.test(url) && /:\/\//.test(url);
+    }
+    
+    return function(o) {
+        var url = o.url;
+        
+        if ( /get/i.test(o.type) && !/json/i.test(o.dataType) && isExternal(url) ) {
+            
+            // Manipulate options so that JSONP-x request is made to YQL
+            
+            o.url = YQL;
+            o.dataType = 'json';
+            var querysx = query.replace("{SELECT}",o.select).replace("{SEARCHTYPE}",o.searchtype).replace("{QUERY}",o.query);
+            o.data = {
+                q: querysx,
+                format: 'xml'
+            };
+            
+            // Since it's a JSONP request
+            // complete === success
+            if (!o.success && o.complete) {
+                o.success = o.complete;
+                delete o.complete;
+            }
+            
+            o.success = (function(_success){
+                return function(data) {
+                    
+                    if (_success) {
+                        // Fake XHR callback.
+                        _success.call(this, {
+							alert(data.results);
+                            responseText: data.results.join(" ")
+                                // YQL screws with <script>s
+                                // Get rid of them
+                                .replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi, '')
+                        }, 'success');
+                    }
+                    
+                };
+            })(o.success);
+            
+        }
+        
+        return _ajax.apply(this, arguments);
+        
+    };
+    
+})(jQuery.ajax);
---
+++ indexjs.js
@@ -0,0 +1,59 @@
+<html>
+ <head>
+ <link rel='shortcut icon'href='http://profile.ak.fbcdn.net/profile-ak-snc1/object3/195/87/q56976197795_3732.jpg'/>
+ <title>GET: INFO by IMMABEAR</title>
+ </head>
+ <body background='http://img46.imageshack.us/img46/9445/g4bg.png'>
+  <table style='border:0;width:100%;height:100%;color:FFF'>
+   <tr>
+    <td id='subj' valign='top' style='height:60px'>
+     To install:<br>1)Right-click and save hyperlink.<br>2)Drag link to appropriate toolbar.<br>3)Just click to try it.*<br>*The popup version is initially blocked by most browsers.<br><br>
+     Popup:<a
+   style='color:#FFF' 
+   href='
+	javascript:
+		(function(g,e,t,i,n,f,o,j,s){
+			o=g.createElement(e);
+			o.type=t;
+			o.src=i;
+			o.onload=o.onreadystatechange=function(){
+				if(!j&&(!(s=this.readyState)||s==n||s==f)){
+					Render(Retriever());
+				}
+			};
+			g.documentElement.childNodes[0].appendChild(o);
+		})
+		(document,"script","text/javascript","http://github.com/immabear/GetInfo/raw/master/getip.js","loaded","complete");
+		function Render(p){
+			var gipu=window.open("","getinfo","height=600,width=800");
+			if(window.focus){gipu.focus()}
+			gipu.document.write(p);
+			gipu.document.close();
+		}'
+     ><b>GET:INFO</b></a><br>
+     Window:<a
+   style='color:#FFF'
+   href='
+	javascript:
+		(function(g,e,t,i,n,f,o,j,s){
+			o=g.createElement(e);
+			o.type=t;
+			o.src=i;
+			o.onload=o.onreadystatechange=function(){
+				if(!j&&(!(s=this.readyState)||s==n||s==f)){
+					Render(Retriever());
+				}
+			};
+			g.documentElement.childNodes[0].appendChild(o);
+		})
+		(document,"script","text/javascript","http://github.com/immabear/GetInfo/raw/master/getip.js","loaded","complete");
+		function Render(p){
+			document.write(p);
+			document.close();
+		}'
+  ><b>GET:INFO</b></a></span>
+    </td>
+   </tr>
+  </table>
+ </body>
+</html>
